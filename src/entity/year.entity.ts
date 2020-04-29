9c239aea5c8f509081fed85bccc3be123e899197
DOligex <olinours@yahoo.fr>
2020-04-29 15:40:59 +0200

    fix: clean useless year fields

--- src/entity/year.entity.ts
+++ src/entity/year.entity.ts
@@ -17,50 +17,8 @@ export class Year {
     @CreateDateColumn({type: 'timestamp'})
     delateAt!: Date;
 
-    @Column({type: 'varchar', length: 50})
-    title!: string;
-    // date de sortie
     @Column({type: 'timestamp'})
-    released!: Date;
-
-    @Column({type: 'int'})
-    runtime!: number;
-
-    @Column({type: 'varchar', length: 50})
-    gender!: string;
-
-    @Column({type: 'varchar', length: 50})
-    director!: string;
-
-    @Column({type: 'varchar', length: 50})
-    writer!: string;
-
-    @Column({type: 'varchar', length: 255})
-    plot!: string;
-
-    @Column({type: 'varchar', length: 25})
-    country!: string;
-
-    @Column({type: 'varchar', length: 150})
-    awards!: string;
-
-    @Column({type: 'varchar', length: 25})
-    production!: string;
-
-    @Column({type: 'boolean', default: false})
-    response!: boolean;
-
-    @Column({type: 'varchar', length: 255})
-    poster!: string;
-
-    @Column({type: 'int'})
-    rating!: number;
-
-    @Column({type: 'varchar', length: 100})
-    comments!: string;
-
-    @Column({type: 'boolean', default: false})
-    warning!: boolean;
+    year!: Date;
 
     @OneToMany(type => Movie, movie => movie.years)
     movies!: Movie[];
