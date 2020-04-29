443879ee0d211583ab8471149770684291aa8644
DOligex <olinours@yahoo.fr>
2020-04-29 15:41:38 +0200

    fix: declare routes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import { YearController } from './controller/year.controller';
+import { MovieController } from './controller/movie.controller';
+import { GenreController } from './controller/genre.controller';
+import { ActorController } from './controller/actor.controller';
 import { UserController } from './controller/user.controller';
 
 import 'reflect-metadata';
@@ -12,7 +16,11 @@ async function startServer() {
     await loaders(app);
 
     // Ajout des différentes route de votre application
+    ActorController(app);
+    GenreController(app);
+    MovieController(app);
     UserController(app);
+    YearController(app);
 
     // Démarrage du serveur une fois que tout est correctement init
     app.listen(3000, () => console.log('Express server  is running'));
